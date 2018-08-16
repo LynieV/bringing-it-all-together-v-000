34c347e7057656f0b256c7afb90b8406dc826b74
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-16 19:58:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,6 +25,12 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.create(name, grade)
+   student = Student.new(name, grade)
+   student.save
+   student
+ end
+
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
@@ -59,6 +65,8 @@ class Dog
   end
 
   def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
